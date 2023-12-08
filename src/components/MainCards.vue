108188230dec77fe23ec266edc90710b5fecd680
Julio Garcia <jgarc5394@gmail.com>
2023-12-08 13:33:45 -0600

    Fixed Search Function

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,14 +1,25 @@
 <script setup>
+    import { ref, computed } from 'vue'
     import useAPI from '@/composables/useAPI'
     import MainCardsSingle from '@/components/MainCardsSingle.vue'
-
+    import MainSearch from '@/components/MainSearch.vue'
+   
     const { characters } = useAPI()
+    const search = ref('')
+
+    const filteredList = computed(() => {
+    return characters.value.filter(character =>
+    character.name.toLowerCase().includes(search.value.toLowerCase())
+  )
+})
 </script>
 
 <template>
-    <div class="sub-wrapper">
+    <input type="text" placeholder="Search Character..." class="search" v-model="search" />
+    
+    <div class="sub-wrapper" v-if="characters">
         <Suspense>
-            <MainCardsSingle v-for="character in characters" :key="characterId" :character="character"/>
+            <MainCardsSingle v-for="character in filteredList" :key="character.characterId" :character="character"/>
             <template #fallback>
                 <div>Loading...</div>
             </template>
@@ -17,6 +28,10 @@
 </template>
 
 <style scoped lang="postcss">
+    .search {
+            @apply rounded-md border-0 px-8 py-4 placeholder-orange-300 text-orange-500 outline-none 
+            focus:ring focus:ring-orange-300;
+        }
     .sub-wrapper {
         @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
     }
